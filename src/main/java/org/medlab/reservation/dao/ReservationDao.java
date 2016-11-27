4a415e8c7ac051733b4816c17cf6751a5572373e
mmelki <mmelki@kapit.fr>
2016-11-27 19:00:13 +0100

    Add a method query to find reservations by flight id

--- src/main/java/org/medlab/reservation/dao/ReservationDao.java
+++ src/main/java/org/medlab/reservation/dao/ReservationDao.java
@@ -3,7 +3,10 @@ package org.medlab.reservation.dao;
 import org.medlab.reservation.entity.Reservation;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationDao extends GenericDao<Reservation, String> {
 
+    List findByFlightID(String flightID);
 }
--- src/main/java/org/medlab/reservation/dao/impl/ReservationDaoImpl.java
+++ src/main/java/org/medlab/reservation/dao/impl/ReservationDaoImpl.java
@@ -5,10 +5,21 @@ import org.medlab.reservation.dao.ReservationDao;
 import org.medlab.reservation.entity.Reservation;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
+import java.util.List;
+
 @Repository
 public class ReservationDaoImpl extends GenericDaoImpl<Reservation, String> implements ReservationDao {
 
     public ReservationDaoImpl() {
         super(Reservation.class);
+    }
+
+    public List findByFlightID(String flightID) {
+        Query query = super.entityManager.createQuery("SELECT r FROM Reservation r " +
+                "JOIN FETCH r.flightInstance fi WHERE fi.flightId = :id");
+        return query.setParameter("id", flightID)
+                .getResultList();
+
     }
 }
