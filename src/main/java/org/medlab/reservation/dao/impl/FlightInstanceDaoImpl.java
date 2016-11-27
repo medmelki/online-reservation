573009be7e942e4381e5ab659e9020be9b76101e
mmelki <mmelki@kapit.fr>
2016-11-27 18:59:19 +0100

    Use IATACode for airport search filter

--- src/main/java/org/medlab/reservation/dao/impl/FlightInstanceDaoImpl.java
+++ src/main/java/org/medlab/reservation/dao/impl/FlightInstanceDaoImpl.java
@@ -17,7 +17,7 @@ public class FlightInstanceDaoImpl extends GenericDaoImpl<FlightInstance, String
 
     public List getAllFlightsByAirportSince(String name, Long time) {
         Query query = super.entityManager.createQuery("SELECT fi FROM FlightInstance fi " +
-                "JOIN FETCH fi.flight f JOIN FETCH f.origin a WHERE fi.date <= :d AND a.name = :n");
+                "JOIN FETCH fi.flight f JOIN FETCH f.origin a WHERE fi.date <= :d AND a.IATACode = :n");
         return query.setParameter("d", time)
                 .setParameter("n", name)
                 .getResultList();
@@ -26,7 +26,7 @@ public class FlightInstanceDaoImpl extends GenericDaoImpl<FlightInstance, String
     public List getAllFlightsByAirportsSince(String origin, String destination, Long time) {
         Query query = super.entityManager.createQuery("SELECT fi FROM FlightInstance fi " +
                 "JOIN FETCH fi.flight f JOIN FETCH f.origin o JOIN FETCH f.destination de " +
-                "WHERE fi.date <= :d AND o.name = :n AND de.name = :n2");
+                "WHERE fi.date <= :d AND o.IATACode = :n AND de.IATACode = :n2");
         return query.setParameter("d", time)
                 .setParameter("n", origin)
                 .setParameter("n2", destination)
